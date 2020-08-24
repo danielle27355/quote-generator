3e96d31993af548d4c68db6845a48cae7da00a55
danielle27355 <danielle27355@gmail.com>
2020-08-24 11:02:38 -0400

    Updates

---
+++ precache-manifest.357cedd58c58259adb5db42163c6081c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089bfdb3650f804a4757d13130ad360f",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "5e8c350d9ad0d840a29e",
+    "url": "/quote-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "335f7a4d53ecdd8c08db",
+    "url": "/quote-generator/static/js/2.f2147cf3.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/quote-generator/static/js/2.f2147cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8c350d9ad0d840a29e",
+    "url": "/quote-generator/static/js/main.7c4d1740.chunk.js"
+  },
+  {
+    "revision": "6e71cb284c179124fe30",
+    "url": "/quote-generator/static/js/runtime-main.8faf9592.js"
+  }
+]);
